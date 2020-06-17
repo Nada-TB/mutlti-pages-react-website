f75655b2e1a9b0fdea86f120955c75c6b8d12ed2
Nada <nada.tebba@hotmail.fr>
2020-06-17 18:22:34 +0200

    Updates

---
+++ precache-manifest.fddf8ad630cfebce7d69932c90de3c9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181a436d3a3f5b116ffdbc6f66191874",
+    "url": "/mutlti-pages-react-website/index.html"
+  },
+  {
+    "revision": "b65dec56593bd766d1c8",
+    "url": "/mutlti-pages-react-website/static/css/main.bb226045.chunk.css"
+  },
+  {
+    "revision": "ee3243c3df8a8acf57d2",
+    "url": "/mutlti-pages-react-website/static/js/2.4d2dbe38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mutlti-pages-react-website/static/js/2.4d2dbe38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65dec56593bd766d1c8",
+    "url": "/mutlti-pages-react-website/static/js/main.dd8d21fc.chunk.js"
+  },
+  {
+    "revision": "da4b4154aabc8a7e4d28",
+    "url": "/mutlti-pages-react-website/static/js/runtime-main.d327331f.js"
+  }
+]);
